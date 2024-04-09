b1963fd56cd7966f993b430d893c064a0fedcf2b
Shijie Wei <112788068+usernameDelet@users.noreply.github.com>
2024-04-08 17:20:17 -0700

    Update Agent.java

--- lab1/Agent.java
+++ lab1/Agent.java
@@ -1,5 +1,6 @@
 package lab1;
 import java.util.Random;
+import java.util.Scanner;
 
 class Agent {
 	
@@ -28,6 +29,98 @@ class Agent {
 	}
 	}
 	
+	static String intToDay(int day) {
+		switch(day) {
+		case 0:
+			return "Sunday";
+		case 1:
+			return "Monday";
+		case 2:
+			return "Tuesday";
+		case 3:
+			return "Wednesday";
+		case 4:
+			return "Thursday";
+		case 5:
+			return "Friday";
+		case 6:
+			return "Saturday";
+		}
+		return null;
+	}
+
+	static void WeekMonthDay() {
+		// question 2
+		//variables
+		String month;
+		String day;
+		int date;
+		
+		// get user input
+		Scanner userInput = new Scanner(System.in);
+		System.out.print("Enter an integer: ");
+		int num = userInput.nextInt();
+		System.out.println("You entered: " + num); 
+		
+		//error checking
+		if (num > 366 || num < 1) {
+			System.out.println("The number you enter is out of range."); 
+		}
+		else {
+			day = intToDay(num % 7);
+			if (num >= 1 && num <= 31) {
+				month = "January";
+				date = num;
+			}
+			else if (num >= 32 && num <= 60) {
+				month = "February";
+				date = (num - 31);
+			}
+			else if (num >= 61 && num <= 91) {
+				month = "March";
+				date = (num - 60);
+			} 
+			else if (num >= 92 && num <= 121) {
+				month = "April";
+				date = (num - 91);
+			}
+			else if (num >= 122 && num <= 152) {
+				month = "May";
+				date = (num - 121);
+			}
+			else if (num >= 153 && num <= 182) {
+				month = "June";
+				date = (num - 152);
+			}
+			else if (num >= 183 && num <= 213) {
+				month = "July";
+				date = (num - 182);
+			}
+			else if (num >= 214 && num <= 244) {
+				month = "August";
+				date = (num - 213);
+			}
+			else if (num >= 245 && num <= 274) {
+				month = "September";
+				date = (num - 244);
+			}
+			else if (num >= 275 && num <= 305) {
+				month = "October";
+				date = (num - 274);
+			}
+			else if (num >= 306 && num <= 335) {
+				month = "November";
+				date = (num - 305);
+			}
+			else {
+				month = "December";
+				date = (num - 335);
+			}
+			
+			System.out.println(day + ", " + month + " " + date); 
+		}
+	}
+
 }
 
 
